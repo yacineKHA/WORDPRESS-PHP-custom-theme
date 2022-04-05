41b87fcf1d09ad13b0737366bdb0db194525ee44
yacineOC <yacine.kad@outlook.fr>
2022-04-05 15:46:35 +0200

    1.3

--- page.php
+++ page.php
@@ -1,12 +1,35 @@
 <?php get_header(); ?>
 
-<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+<?php
+// Boucle principale
+	if( have_posts() ) : while( have_posts() ) : the_post();
+		the_title(); // Titre de la page
 
-	<h1><?php the_title(); ?></h1>
+		$my_query = new WP_Query( array( 'post_type' => 'post' ) );
 
-	<?php the_content(); ?>
+		// Boucle personnalisée
+		if( $my_query->have_posts() ) : while( $my_query->have_posts() ) :
+			$my_query->the_post();
 
-<?php endwhile; endif; ?>
+			the_title(); // Titre de chaque article
+			the_content(); // Contenu de chaque article
+
+		endwhile; endif;
+		wp_reset_postdata(); // On réinitialise les données
+		//N’oubliez donc jamais de terminer vos requêtes personnalisées
+        //par wp_reset_postdata afin de retourner dans la boucle principale.
+
+		the_content(); // Contenu de la page
+
+	endwhile; endif;
+
+//if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+    <!---->
+    <!--	<h1>--><?php //the_title(); ?><!--</h1>-->
+    <!---->
+    <!--	--><?php //the_content(); ?>
+    <!---->
+<?php //endwhile; endif; ?>
 
 
 
